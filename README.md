bc9dacbd411e392a5243ab7bcb501bd3b0d45dc5
linhpvt60 <linhpvt60@gmail.com>
2021-09-18 16:53:36 +0700

    Init duxable using redux-observable and hooks

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React - Redux - Observable [redux-observable]
+
+# Hooks
---
+++ duxable/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Welcome to Redux-Obserbable and Hooks.</p>
+        Keep growing and keep learning
+      </header>
+    </div>
+  );
+}
+
+export default App;
